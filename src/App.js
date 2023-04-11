2cfdbc53aa90176dd5251d75d2c81a3e359d2f9a
Your Name <you@example.com>
2023-04-11 00:42:01 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import profile from './Image/profile.jpeg';
+import Home from './Component/Home';
+function App() {
+  return (
+<>
+<div className='profile'>
+<img src={profile} className='profile'/>
+</div>
+<Home/>
+</> 
+
+
+);
+}
+
+export default App;
---
+++ src/Component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2>Hi Everyone,</h2>
+      <h2>I'm Shyam sundar Velayuham </h2>
+      <h2>Front End UI Developer</h2>
+    </div>
+  )
+}
+
+export default Home
